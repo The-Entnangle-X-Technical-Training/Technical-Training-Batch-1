268214071fa11822aaf0ade36337be9d52a0e5f5
Sumit Dhiwar <sumitdhiwar053@gmail.com>
2026-02-06 10:06:53 +0530

    test file added

---
+++ first.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number = 10;
+    cout<<number<<endl;
+
+    number = 20;
+    cout<<number<<endl;
+
+    number = number + 5;
+    cout<<number<<endl;
+
+    number = number *2;
+    cout<<number<<endl;
+
+    return 0;
+}
