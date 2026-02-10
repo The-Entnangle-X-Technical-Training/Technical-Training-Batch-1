fddb2ab7ef763785098832f14c38933db8f7c6c5
dishaa05 <dishajhinjhore0505@gmail.com>
2026-02-10 21:15:04 +0530

    Add files via upload

---
+++ Test Problems/P2.cpp
@@ -0,0 +1,21 @@
+//Nearest Number Divisible by Y
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int X;
+    int Y;
+    int closestNumber;
+    
+    cout<<"X:";
+    cin>>X;
+
+    cout<<"Y:";
+    cin>>Y;
+
+    closestNumber=X-(X%Y);
+    cout<<closestNumber;
+
+}
