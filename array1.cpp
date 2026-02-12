6f96d3cb5b287d56ffef67436c2f1e597dffbb70
Nitanshi Rathod <nitanshirathod@gmail.com>
2026-02-12 17:26:50 +0530

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Array elements: ";
+    printArray(arr, n);
+
+    return 0;
+}
