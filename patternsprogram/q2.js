8dcf1bbcbd7bd4d7c996d7efcfbc58562fef7c4b
Rankita Barela <barelarankita@gmail.com>
2026-02-12 19:17:08 +0530

    patterns printng in js

---
+++ patternsprogram/q2.js
@@ -0,0 +1,11 @@
+//rectangle pattern(m*n)
+
+// Write a program that prints a 4×4 square pattern with stars.
+for(let i=1;i<=3;i++){
+    row = " ";
+    for(let j= 1;j<=6;j++){
+        row +="*";
+        
+        }
+        console.log(row);
+    }
---
+++ patternsprogram/q3.js
@@ -0,0 +1,11 @@
+// right tringle Star (increasing)
+
+
+for(let i = 1;i<=4;i++){
+    row=" ";
+
+    for(let j = i;j>=1;j--){
+        row +="*";
+    }
+    console.log(row);
+}
